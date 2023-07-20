fa588ec0b67acbaff669954542ef2906e0a51eea
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-07-20 16:17:20 +0600

    chapter 3 and 4 done

---
+++ next03/app/users/page.tsx
@@ -0,0 +1,36 @@
+import getAllUsers from "@/lib/getAllUsers";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Users",
+};
+
+import React from "react";
+
+export default async function UsersPage() {
+  const usersData: Promise<User[]> = getAllUsers();
+
+  const users = await usersData;
+  
+
+  const content = (
+    <section>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+      <br />
+      {users.map((user) => {
+        return (
+          <>
+            <p key={user.id}>
+              <Link href={`/users/${user.id}`}>{user.name}</Link>
+            </p>
+            <br />
+          </>
+        );
+      })}
+    </section>
+  );
+  return content;
+}
